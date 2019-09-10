23d6da0cd91314f6b7483874ea679d8648e15018
Christopher Freytes <cfreytes@bu.edu>
2019-09-10 19:18:37 -0400

    Revised Variables

--- assignment_2/src/com/freytes/Main.java
+++ assignment_2/src/com/freytes/Main.java
@@ -1,15 +1,6 @@
 package com.freytes;
 public class Main {
 
-	//Declare Variables
-	String fn1;
-	String ln1;
-	//String[] ln1 = new String[0];
-	String rname;
-	int[] n1 = new int[7];
-	int finNumbervar = 7;
-	boolean errormsg = false;
-
 	public static void main(String[] args) {
 		//Calling alphaParser Class
 		alphaParser callClass = new alphaParser();
