51f4ca43b33cea1bd17a0c777df1b2e6226bccbb
Christopher Freytes <cfreytes@bu.edu>
2019-09-11 10:10:24 -0400

    - Added a input verification class.

--- assignment_2/src/com/freytes/inputVerification.java
+++ assignment_2/src/com/freytes/inputVerification.java
@@ -1,4 +1,7 @@
 package com.freytes;
 
 public class inputVerification {
+    public boolean specialCharacters(String individualChar) {
+        return (individualChar == null) || "".equals(individualChar.trim()) || individualChar.matches(("\\d+"));
+    }
 }
