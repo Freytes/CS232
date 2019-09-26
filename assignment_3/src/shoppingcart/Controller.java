85535e83e85e1b6e3e7ff2ca4a0a55716ae510bf
Christopher Freytes <cfreytes@bu.edu>
2019-09-26 01:04:41 -0400

    Added addItem method

--- assignment_3/src/shoppingcart/Controller.java
+++ assignment_3/src/shoppingcart/Controller.java
@@ -1,27 +1,39 @@
 package shoppingcart;
 
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
-    //Initialize variables
-    public Button itemAdd;
 
-    //The Initializer used to load data prior to loading view.
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println("Loading user data.");
+
+    public void displayItems() {
+
+        Label cartEmpty = new Label("Currently Empty");
+        VBox mainScreen_Layout = new VBox(20);
+        mainScreen_Layout.getChildren().addAll(cartEmpty);
+
     }
 
     public void addItems() {
         System.out.println("Displaying addItem page");
+
     }
 
     public void handleitemAdd() {
         System.out.println("Added item to cart test");
     }
+
+    //The Initializer used to load data prior to loading view.
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println("Loading user data.");
+
+
+    }
+
 
 }
--- assignment_3/src/shoppingcart/Main.java
+++ assignment_3/src/shoppingcart/Main.java
@@ -4,9 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    Stage window;
+
 
     public static void main(String[] args) {
         launch(args);
@@ -14,9 +18,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+
+        window.setTitle("Shopping Cart");
+
         Parent root = FXMLLoader.load(getClass().getResource("shoppingcart.fxml"));
-        primaryStage.setTitle("Shopping Cart");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+
+
+        window.setScene(new Scene(root, 300, 275));
+        window.show();
     }
+
 }
