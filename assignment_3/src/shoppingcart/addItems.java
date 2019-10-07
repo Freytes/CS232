644032676acfcbe383ede0252ca72b6ce551b34c
Christopher Freytes <cfreytes@bu.edu>
2019-10-07 16:16:11 -0400

    Added Input Verification

--- assignment_3/src/shoppingcart/addItems.java
+++ assignment_3/src/shoppingcart/addItems.java
@@ -8,15 +8,14 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 import static shoppingcart.shoppingCartController.products;
@@ -25,8 +24,11 @@ import static shoppingcart.shoppingCartController.products;
 public class addItems implements Initializable {
 
     // Priority Choice Values
-    private String[] priorityChoice =
-            {"1", "2", "3", "4", "5", "6", "7"};
+   private String[] priorityChoice =
+           {"1", "2", "3", "4", "5", "6", "7"};
+
+    //ArrayList<String> priorityChoice = new ArrayList<>();
+
 
     // Fields used to add items to cart//
     @FXML
@@ -37,8 +39,6 @@ public class addItems implements Initializable {
     private TextField productPrice = new TextField();
     @FXML
     private ComboBox<String> productPriority = new ComboBox<String>(FXCollections.observableArrayList(Arrays.toString(priorityChoice)));
-
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -54,46 +54,84 @@ public class addItems implements Initializable {
         Scene shoppingCart_scene = new Scene(loader.load());
         shoppingCartController controller = loader.getController();
 
-        products.addAll(new Products(productPriority.getValue(),
-                productName.getText(),
-                Double.parseDouble(productPrice.getText()),
-                Integer.parseInt(productQty.getText())));
+     if(isInputValid()){
+         products.addAll(new Products(productPriority.getValue(),
+                 productName.getText(),
+                 Double.parseDouble(productPrice.getText()),
+                 Integer.parseInt(productQty.getText())));
 
-        controller.loadData(products);
+         controller.loadData(products);
 
-        System.out.println("Displaying information to consoles: Ensuring the addItem method worked as expected.");
-    }
+         // Clear values
+         productPriority.setPromptText("Please Select Priority");
+         productName.clear();
+         productPrice.clear();
+         productQty.clear();
 
-    public void handleitemReturnCart(ActionEvent event) throws IOException {
+     }
 
-        Parent shoppingCart_page = FXMLLoader.load(getClass().getResource("shoppingcart.fxml"));
-        Scene shoppingCart_scene = new Scene(shoppingCart_page);
-        Stage shoppingCart_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        shoppingCart_stage.setScene(shoppingCart_scene);
-        shoppingCart_stage.show();
+            System.out.println("Displaying information to consoles: Ensuring the addItem method worked as expected.");
+        }
 
-        System.out.println("Displaying information to console: Ensuring that user returned to main page");
+        public void handleitemReturnCart (ActionEvent event) throws IOException {
 
-    }
+            Parent shoppingCart_page = FXMLLoader.load(getClass().getResource("shoppingcart.fxml"));
+            Scene shoppingCart_scene = new Scene(shoppingCart_page);
+            Stage shoppingCart_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            shoppingCart_stage.setScene(shoppingCart_scene);
+            shoppingCart_stage.show();
 
-    public String arrayItems() {
+            System.out.println("Displaying information to console: Ensuring that user returned to main page");
 
-/*            for(int i = 0; i < priorityChoice.length; i++) {
-                if(priorityChoice[i].equals(productPriority)) {
-                    priorityChoice[i] = null;
-                    priorityChoice.removeAllItems();
-                    priorityChoice.addItem(productPriority);
+        }
+
+        public String arrayItems () {
+
+            //Used to Initialize the Scene
+            productPriority.getItems().addAll(priorityChoice);
+            return String.valueOf(productPriority);
+        }
+
+        public boolean isInputValid() {
+            String errorMessage = "";
+
+            if (productName.getText() == null || productName.getText().length() == 0 || !productName.getText().matches("\"|[-\\\\+]?|[-\\\\+]?\\\\d+\\\\.?|[-\\\\+]?\\\\d+\\\\.?\\\\d+\"")) {
+                errorMessage += "Not a valid Product Name!\n";
+            }
+            if (productPrice.getText() == null || productPrice.getText().length() == 0) {
+                errorMessage += "Not a valid Product Price!\n";
+            } else {
+                // try to parse the postal code into an int.
+                try {
+                    Integer.parseInt(productPrice.getText());
+                } catch (NumberFormatException e) {
+                    errorMessage += "Not a valid Product Price (must be an integer)!\n";
+                }
+            }
+            if (productQty.getText() == null || productQty.getText().length() == 0) {
+                errorMessage += "Not a valid Product Qty!\n";
+            } else {
+                // try to parse the postal code into an int.
+                try {
+                    Integer.parseInt(productQty.getText());
+                } catch (NumberFormatException e) {
+                    errorMessage += "Not a valid Product Qty (must be an integer)!\n";
                 }
             }
 
-            if (priorityChoice.getSelectedIndex() > -1) {
-                productPriority.remove(priorityChoice.getSelectedIndex());
-                productPriority.getSelectedIndex();
-            }*/
+            if (errorMessage.length() == 0) {
+                return true;
+            } else {
+                // Show the error message.
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Invalid Fields");
+                alert.setHeaderText("Please correct invalid fields");
+                alert.setContentText(errorMessage);
 
+                alert.showAndWait();
 
-        //Used to Initialize the Scene
-        productPriority.getItems().addAll(priorityChoice);
-        return String.valueOf(productPriority);
+                return false;
+            }
+        }
+
     }
-}
--- assignment_3/src/shoppingcart/shoppingCartController.java
+++ assignment_3/src/shoppingcart/shoppingCartController.java
@@ -9,6 +9,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -27,8 +28,9 @@ public class shoppingCartController implements Initializable {
     @FXML private TableColumn<Products, String> item_Name;
     @FXML private TableColumn<Products, Number> item_Qty;
     @FXML private TableColumn<Products, Number> item_Price;
-    @FXML
-    private TextField productGrandtotal = new TextField();
+    @FXML private TextField productGrandtotal = new TextField();
+    @FXML private TextField cartBudget = new TextField();
+
 
     //Set as static so it can be shared between classes
     static ObservableList<Products> products = FXCollections.observableArrayList();
@@ -51,6 +53,7 @@ public class shoppingCartController implements Initializable {
 
     //Sets arraylist and adds items//
     public void loadData(ObservableList<Products> products) {
+        item_Table.refresh();
         item_Table.getItems().addAll(products);
     }
 
@@ -69,6 +72,29 @@ public class shoppingCartController implements Initializable {
 
     //Deletes items
     public void handleitemDelete(ActionEvent event) throws IOException {
+
+        int selectedIndex = item_Table.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            products.removeAll(products);
+            item_Table.getItems().clear();
+            item_Table.refresh();
+
+        } else {
+            // Nothing selected.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Product Selected");
+            alert.setContentText("Please select an item from the table.");
+
+            alert.showAndWait();
+        }
+
+
+
+
+
+
+
         System.out.println("Displaying information to consoles: Deleting Selected Item");
-    }
+        }
 }
